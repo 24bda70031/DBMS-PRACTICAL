dd2ebd25dd1ca730c596b741d58f4a1c7c85b8c3
24BDA70031 <168569554+24bda70031@users.noreply.github.com>
2026-01-22 09:41:01 +0530

    Add SQL script for Students table and queries
    Created a SQL script to manage Students table with various queries.

---
+++ experiment 2/experiment2.sql
@@ -0,0 +1,40 @@
+CREATE TABLE Students (
+    id NUMERIC PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(30),
+    marks NUMERIC(10)
+);
+
+INSERT INTO Students VALUES (1, 'Aman', 'Mohali', 85);
+INSERT INTO Students VALUES (2, 'Rohit', 'Mohali', 78);
+INSERT INTO Students VALUES (3, 'Neha', 'Mohali', 92);
+INSERT INTO Students VALUES (4, 'Simran', 'Amritsar', 88);
+INSERT INTO Students VALUES (5, 'Karan', 'Amritsar', 75);
+
+SELECT * FROM Students
+
+--i
+SELECT CITY, COUNT(*) AS COUNT_OF_STUDENTS
+FROM Students
+GROUP BY CITY
+
+--ii
+SELECT CITY, COUNT(Id) AS COUNT_OF_STUDENTS
+FROM Students
+GROUP BY CITY
+
+--iii
+SELECT CITY, COUNT(Id) AS COUNT_OF_STUDENTS
+FROM Students
+GROUP BY CITY
+HAVING COUNT(Id)<3
+
+SELECT city,AVG(MARKS)::NUMERIC(10,2) as average_marks
+FROM Students
+Group BY city;
+
+SELECT city, SUM(marks) FROM Students GROUP BY city;
+
+SELECT city, MAX(marks) FROM Students GROUP BY city;
+
+SELECT city, MIN(marks) FROM Students GROUP BY city;
